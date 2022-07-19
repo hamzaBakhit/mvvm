1ed26fb2d5b2ef1701b3c37b913b1a484c642d95
hamza bakhit <77640698+hamzaBakhit@users.noreply.github.com>
2022-07-20 00:12:54 +0300

    i put 3rd commit...

--- lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
@@ -7,6 +7,7 @@ import '../../base/base_view_model.dart';
 import '../../resources/assets_manager.dart';
 import '../../resources/strings_manager.dart';
 //OnBoardingViewModel ...
+//my name is hamza
 class OnBoardingViewModel extends BaseViewModel
     with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
   // stream controllers outputs
